56fa026b9ef2b02002db5ab537b36f116599fd29
Konstantin <nzitprofessional@gmail.com>
2016-12-07 15:56:22 +1300

    Revert "Final interface upload."
    This reverts commit 444df9bd50bde30a31acb86fe8cdfad59f9aec41.

--- WebInterfaceBetta/www/js/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// BroadbandCompare App
-var app = angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives', 'app.services']);
-// 
-var appControllers = angular.module('app.controllers', []);
-var appServices = angular.module('app.services', []);
-var appDirectives = angular.module('app.directives', []);
